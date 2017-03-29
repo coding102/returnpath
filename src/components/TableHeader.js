ad0162cfef55b4c0a1647f79c89710b703a1cc95
Marck <mercercountynj@gmail.com>
2017-03-29 01:27:38 -0400

    typo

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -5,8 +5,6 @@ class TableHeader extends Component {
   render() {
     return (
         <div>   
-            <h1>Your Email Organized</h1>
-            <h3>You can recategorize your senders or move them to your inbox by unchecking the box next to the senders name.</h3>
         </div>
     );
   }
