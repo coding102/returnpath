8f5ed6ad26ecd12c48288725903b00c08fab78db
Marck <mercercountynj@gmail.com>
2017-03-30 02:29:43 -0400

    tableheader styling

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -4,8 +4,43 @@ import './TableHeader.css';
 class TableHeader extends Component {
   render() {
     return (
-        <div>   
-        </div>
+      <div className="container">
+
+        <table className="table">
+          <thead>
+            <tr>
+                <th style={{height: '50px'}}>Organize</th>
+                <th style={{height: '50px'}}>Sender</th>
+                <th style={{height: '50px'}}>Domain</th>
+                <th style={{height: '50px'}}>Email</th>
+                <th style={{height: '50px'}}>Folder</th>
+            </tr>
+
+          </thead>
+          <tbody>
+            <tr>
+                <th>
+                  <input type="checkbox" name="" value="" />
+                </th>
+
+                <th style={{backgroundColor: 'white'}}>Sender</th>
+                <th style={{backgroundColor: 'white'}}>Domain</th>
+                <th style={{backgroundColor: 'white'}}>Email</th>
+                <th style={{backgroundColor: 'white'}} className="bordercolor">
+                  <select style={{height: '30px', width: '100%'}}>
+                    <option>Travel</option>
+                    <option>Shopping</option>
+                    <option>Finance</option>
+                  </select>
+                </th>
+            </tr>
+          </tbody>
+
+
+             
+            
+        </table>
+      </div>
     );
   }
 }
